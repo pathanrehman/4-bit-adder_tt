c9e893a7c655f8b71150e01c4c4b885d7e71e7a7
pathanrehman <52851945+pathanrehman@users.noreply.github.com>
2025-08-26 10:48:24 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,19 +1,15 @@
 `default_nettype none
 `timescale 1ns / 1ps
 
-/* This testbench just instantiates the module and makes some convenient wires
-   that can be driven / tested by the cocotb test.py.
-*/
 module tb ();
-
-  // Dump the signals to a VCD file. You can view it with gtkwave or surfer.
+  // Dump the signals to a VCD file
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
     #1;
   end
 
-  // Wire up the inputs and outputs:
+  // Wire up the inputs and outputs
   reg clk;
   reg rst_n;
   reg ena;
@@ -22,28 +18,21 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
+
+  // Replace tt_um_example with your module name
+  tt_um_4bit_cpu user_project (
+    // Include power ports for the Gate Level test
 `ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
+      .VPWR(1'b1),
+      .VGND(1'b0),
 `endif
-
-  // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
-      // Include power ports for the Gate Level test:
-`ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
-`endif
-
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
-      .ena    (ena),      // enable - goes high when design is selected
-      .clk    (clk),      // clock
-      .rst_n  (rst_n)     // not reset
+      .ui_in(ui_in),    // Dedicated inputs
+      .uo_out(uo_out),  // Dedicated outputs
+      .uio_in(uio_in),  // IOs: Input path
+      .uio_out(uio_out), // IOs: Output path
+      .uio_oe(uio_oe),  // IOs: Enable path (active high: 0=input, 1=output)
+      .ena(ena),        // enable - goes high when design is selected
+      .clk(clk),        // clock
+      .rst_n(rst_n)     // not reset
   );
-
 endmodule
